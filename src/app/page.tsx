9e119eced2a01c3525ae8ca2d4136d047bb21d1d
Medved Dima <dimam@assuta.co.il>
2025-02-06 18:28:21 +0200

    home page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { Button } from '@/components/button/button';
 
 type HomePageProps = {};
 
 const HomePage = (props: HomePageProps) => {
   return (
-    <div>
-      <h1>HOME PAGE</h1>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <Button url='/portfolio' text='See Our Works' />
+      </div>
+      <div className={styles.item}>
+        {/* <Image src={'/hero.png'} alt='' className={styles.img} fill /> */}
+      </div>
     </div>
   );
 };
