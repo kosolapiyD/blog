52a9aba61bb28d8a116ba16a374f7d3872bca669
Medved Dima <dimam@assuta.co.il>
2025-02-06 19:24:57 +0200

    about page content and style

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,11 +1,34 @@
+import { Button } from '@/components/button/button';
 import styles from './page.module.scss';
+import Image from 'next/image';
 
 type ContactUsProps = {};
 
 const ContactUs = (props: ContactUsProps) => {
   return (
     <div className={styles.container}>
-      <h1>ContactUs Page</h1>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src='/contact.png'
+            alt=''
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type='text' placeholder='name' className={styles.input} />
+          <input type='text' placeholder='email' className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder='message'
+            cols={30}
+            rows={10}
+          ></textarea>
+          <Button url='#' text='Send' />
+        </form>
+      </div>
     </div>
   );
 };
